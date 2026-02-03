ad2cbb5f03be1dec6a002dc34fd54ed2b3515165
Rajkumar <106533527+gmrajkumar123@users.noreply.github.com>
2026-02-03 15:58:29 -0800

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,11 +1,11 @@
 terraform {
-  # backend "s3" {
-  #   bucket       = "dev-raj-tf-bucket"
-  #   region       = "ap-south-1"
-  #   key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-  #   encrypt      = true
-  #   use_lockfile = true
-  # }
+   backend "s3" {
+     bucket       = "dev-raj-tf-bucket"
+     region       = "ap-south-1"
+     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+     encrypt      = true
+     use_lockfile = true
+   }
   required_version = ">=1.14.0"
   required_providers {
     aws = {
